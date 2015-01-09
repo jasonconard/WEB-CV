5953a6f2ad5a305781a7ac103e3d35b690ac212c
Jason Conard <jconard@excilys.com>
2015-01-09 17:22:13 +0100

    Begin style for canvas

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -8,8 +8,27 @@
  * Controller of the webcvApp
  */
 
-var MainCtrl = function($rootScope, $scope) {
-console.log('Hello World');
+var MainCtrl = function($rootScope, $scope, $timeout) {
+  $scope.switchCanvas = true;
+
+  $timeout(function() {
+
+    var canvasObject = $('#header-canvas')[0];
+    var canvasCurrentWidth = canvasObject.clientWidth;
+    var ctx = canvasObject.getContext("2d");
+
+
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo(0,canvasCurrentWidth);
+    ctx.lineTo(1000,canvasCurrentWidth);
+    ctx.lineTo(1000,0);
+    ctx.closePath();
+    ctx.fillStyle = "lightblue";
+    ctx.fill();
+
+  },100);
+
 };
 
-angular.module('webcvApp').controller('MainCtrl', ['$rootScope', '$scope', MainCtrl]);
+angular.module('webcvApp').controller('MainCtrl', ['$rootScope', '$scope', '$timeout', MainCtrl]);
