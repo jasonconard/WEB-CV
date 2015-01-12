7b0e31a4d84952b3a1c1e8a7cd8907baab5adcec
Jason Conard <jconard@excilys.com>
2015-01-12 18:10:10 +0100

    Header buttons for pages

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -11,6 +11,31 @@
 var MainCtrl = function($rootScope, $scope, $timeout) {
   $scope.switchCanvas = true;
 
+  $scope.menu = [
+    {
+      icon : 'user',
+      text : 'label.menu-profile'
+    },{
+      icon : 'coffee',
+      text : 'label.menu-professional'
+    },{
+      icon : 'graduation-cap',
+      text : 'label.menu-diplomas'
+    },{
+      icon : 'gears',
+      text : 'label.menu-projects'
+    },{
+      icon : 'wrench',
+      text : 'label.menu-skills'
+    },{
+      icon : 'gamepad',
+      text : 'label.menu-hobbys'
+    },{
+      icon : 'camera-retro',
+      text : 'label.menu-album'
+    }
+  ];
+
   $timeout(function() {
 
     var canvasObject = $('#header-canvas')[0];
